95be8500a1be65ae9c6a04a10f680a0f15345baf
Josh Febbie <joshdfebbie@gmail.com>
2022-11-03 14:32:50 -0700

    firstwed-labpush

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require("dotenv").config()
+
+app.use(express.json())
+
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
+
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
